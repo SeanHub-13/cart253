ee55ccb0aaf9aaeb37a6a02c7dcd4693ebfa70c7
SeanHub-13 <coolflame68@gmail.com>
2024-09-19 12:37:34 -0400

    Update script.js

--- topics/ufo-on-a-dark-night/js/script.js
+++ topics/ufo-on-a-dark-night/js/script.js
@@ -33,9 +33,22 @@ function setup() {
  * Displays a UFO
 */
 function draw() {
+    // Sky shade goes up
+    // The sky gradually turns bright white!
+    skyShade = skyShade + 1;
     // Display the sky
     background(skyShade);
-
+    // The UFO goes up
+    // We have lift-off!
+    ufo.y = ufo.y - 2;
+    ufo.x = ufo.x + 0.5
+    // UFO gradually becomes darker
+    // I mean, activating quantum thrust transition...
+    ufo.fill = ufo.fill * 0.999;
+    // UFO's dimensions become smaller over time
+    // ... as it flies away into the distance! ET... gone home...
+    ufo.width = ufo.width / 1.005;
+    ufo.height = ufo.height / 1.005;
     // Draw the UFO based on its properties
     push();
     fill(ufo.fill);
